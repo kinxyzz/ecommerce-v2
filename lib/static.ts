8c20eb9d7edfa4b4bdc473ca6f78a54906d4c6df
kinxyzz <kinxyzz@gmail.com>
2024-06-10 17:51:51 +0700

    fix:fetchurl

--- lib/static.ts
+++ lib/static.ts
@@ -1,2 +1,2 @@
 // export const serverUrl = "http://localhost:5500";
-export const serverUrl = "https://express-ecommerce-brown.vercel.app/api";
+export const serverUrl = "https://express-ecommerce-brown.vercel.app";
