74c2f1bedb45ee5b4222285b9f60223b4df079b0
kinxyzz <kinxyzz@gmail.com>
2024-06-11 15:41:01 +0700

    huh

--- lib/static.ts
+++ lib/static.ts
@@ -1,2 +1,2 @@
-export const serverUrl = "http://localhost:5500";
-// export const serverUrl = "https://express-ecommerce-brown.vercel.app";
+// export const serverUrl = "http://localhost:5500";
+export const serverUrl = "https://express-ecommerce-brown.vercel.app";
