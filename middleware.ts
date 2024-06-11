e2c78705a1abf97a9a07692f1a170fe75be215de
kinxyzz <kinxyzz@gmail.com>
2024-06-11 16:55:29 +0700

    middleware-check

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  const refreshTokenCookie = req.cookies.get("refreshToken")?.value;
-  console.log("This is your refreshToken: " + refreshTokenCookie);
-}
+export async function middleware(req: NextRequest) {}
 
 export const config = {
   matcher: ["/admin", "/login", "/register"],
