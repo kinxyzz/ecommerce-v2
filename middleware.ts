a37bb98654ad53c035a4ee7abb66d6d33607a87e
kinxyzz <kinxyzz@gmail.com>
2024-06-11 16:14:19 +0700

    testing

--- middleware.ts
+++ middleware.ts
@@ -4,44 +4,44 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const refreshTokenCookie = req.cookies?.get("refreshToken")?.value;
-  const allcookie = req.cookies.getAll();
-  console.log(allcookie);
-  console.log("this is your refreshtoken" + refreshTokenCookie);
+  const response = NextResponse.next();
+  response.headers.set("x-middleware-cache", "no-store");
 
+  console.log("This is your refreshToken: " + refreshTokenCookie);
   try {
     if (!refreshTokenCookie) {
       if (url.pathname === "/admin") {
         url.pathname = "/";
         return NextResponse.redirect(url);
       }
-      return NextResponse.next();
+      return response;
     }
 
     const decoded = jwt.decode(refreshTokenCookie) as jwt.JwtPayload | null;
 
     if (!decoded || decoded.role !== "ADMIN") {
-      const protectedUrl = ["/admin", "/login", "/register"];
-      if (protectedUrl.includes(url.pathname)) {
+      const protectedUrls = ["/admin", "/login", "/register"];
+      if (protectedUrls.includes(url.pathname)) {
         url.pathname = "/";
         return NextResponse.redirect(url);
       }
     }
 
-    if (!decoded || decoded.role === "USER") {
+    if (decoded && decoded.role === "USER") {
       if (url.pathname === "/login" || url.pathname === "/register") {
         url.pathname = "/";
         return NextResponse.redirect(url);
       }
     }
 
-    return NextResponse.next();
+    return response;
   } catch (error) {
     console.error("Error decoding JWT:", error);
     if (url.pathname === "/admin") {
       url.pathname = "/";
       return NextResponse.redirect(url);
     }
-    return NextResponse.next();
+    return response;
   }
 }
 
