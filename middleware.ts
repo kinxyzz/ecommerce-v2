60e1480d75c840e9e0730b3634252bafb0fde391
kinxyzz <kinxyzz@gmail.com>
2024-06-12 15:52:59 +0700

    eaea

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,46 @@
-// middleware.js
-import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  const cookies = request.cookies.getAll();
-  console.log(cookies);
+export async function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+  const refreshTokenCookie = req.cookies.get("refreshToken")?.value;
+
+  if (!refreshTokenCookie) {
+    if (url.pathname === "/admin") {
+      url.pathname = "/";
+      return NextResponse.redirect(url);
+    }
+    return NextResponse.next();
+  }
+
+  try {
+    const decoded = jwt.decode(refreshTokenCookie) as jwt.JwtPayload | null;
+
+    if (!decoded || decoded.role !== "ADMIN") {
+      if (url.pathname === "/admin") {
+        url.pathname = "/";
+        return NextResponse.redirect(url);
+      }
+    }
+
+    if (!decoded || decoded.role === "USER") {
+      if (url.pathname === "/login" || url.pathname === "/register") {
+        url.pathname = "/";
+        return NextResponse.redirect(url);
+      }
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Error decoding JWT:", error);
+    if (url.pathname === "/admin") {
+      url.pathname = "/";
+      return NextResponse.redirect(url);
+    }
+    return NextResponse.next();
+  }
 }
+
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/login"],
+  matcher: ["/admin", "/login", "/register"],
 };
