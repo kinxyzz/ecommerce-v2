3efdcbeef6c435d581d0b24d69412e9b8162c273
kinxyzz <kinxyzz@gmail.com>
2024-06-18 20:47:06 +0700

    whhe

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           <Navbar />
           <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
