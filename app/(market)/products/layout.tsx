5ff2b883224a52bc69075a06585539a104f02df7
kinxyzz <kinxyzz@gmail.com>
2024-06-18 20:29:18 +0700

    mboh raurus

--- app/(market)/products/layout.tsx
+++ app/(market)/products/layout.tsx
@@ -1,3 +1,9 @@
+import { Suspense } from "react";
+
 export default function layout({ children }: { children: React.ReactNode }) {
-  return <main className="container mx-auto py-4 w-full">{children}</main>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="container mx-auto py-4 w-full">{children}</main>;
+    </Suspense>
+  );
 }
--- app/(market)/products/page.tsx
+++ app/(market)/products/page.tsx
@@ -1,6 +1,5 @@
 import FilterProduct from "@/components/fragment/FilterProduct";
 import ProductList from "@/components/template/product/ProductList";
-import { Suspense } from "react";
 
 const filterOption = [
   {
@@ -19,18 +18,16 @@ const filterOption = [
 
 export default function page() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <section className="flex gap-4 w-full ">
-        <div className="shrink min-w-72 border-r hidden lg:flex">
-          <div className="flex flex-col gap-12">
-            <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-              Filter Products
-            </h2>
-            <FilterProduct filterField="material" options={filterOption} />
-          </div>
+    <section className="flex gap-4 w-full ">
+      <div className="shrink min-w-72 border-r hidden lg:flex">
+        <div className="flex flex-col gap-12">
+          <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            Filter Products
+          </h2>
+          <FilterProduct filterField="material" options={filterOption} />
         </div>
-        <ProductList />
-      </section>
-    </Suspense>
+      </div>
+      <ProductList />
+    </section>
   );
 }
